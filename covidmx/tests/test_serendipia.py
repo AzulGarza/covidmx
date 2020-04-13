9c2a9976f2a7d4689d9586c8ece98c8ec7a8acfc
Fede <fede.garza.ramirez@gmail.com>
2020-04-12 20:47:05 -0500

    new tests

--- covidmx/tests/test_serendipia.py
+++ covidmx/tests/test_serendipia.py
@@ -5,5 +5,8 @@ from covidmx import CovidMX
 def test_returns_data():
     try:
         covid_data = CovidMX().get_data()
+        raw_data = CovidMX(clean=False).get_data()
+        confirmed = CovidMX(kind="confirmed").get_data()
+        suspects = CovidMX(kind="suspects").get_data()
     except BaseException:
         assert False, "Test failed"
