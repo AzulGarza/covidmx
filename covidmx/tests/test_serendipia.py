65101de7e63c6c6f6a176895cd2ca43220a4d972
Fede <fede.garza.ramirez@gmail.com>
2020-04-13 23:46:49 -0500

    updating serendipia test

--- covidmx/tests/test_serendipia.py
+++ covidmx/tests/test_serendipia.py
@@ -5,8 +5,8 @@ from covidmx import CovidMX
 def test_returns_data():
     try:
         covid_data = CovidMX(source='Serendipia').get_data()
-        raw_data = CovidMX(clean=False).get_data()
-        confirmed = CovidMX(kind="confirmed").get_data()
-        suspects = CovidMX(kind="suspects").get_data()
+        raw_data = CovidMX(source='Serendipia', clean=False).get_data()
+        confirmed = CovidMX(source='Serendipia', kind="confirmed").get_data()
+        suspects = CovidMX(source='Serendipia', kind="suspects").get_data()
     except BaseException:
         assert False, "Test Serendipia failed"
