1440d28528c61900f2e0d65fd8b9961f0fd7399f
Fede <fede.garza.ramirez@gmail.com>
2020-04-20 10:49:30 -0500

    adding new tests

--- covidmx/tests/test_dge.py
+++ covidmx/tests/test_dge.py
@@ -9,5 +9,7 @@ def test_returns_data():
         covid_dge_data, catalogo_data = CovidMX(return_catalogo=True).get_data()
         covid_dge_data, descripcion_data = CovidMX(return_descripcion=True).get_data()
         covid_dge_data, catalogo_data, descripcion_data = CovidMX(return_catalogo=True, return_descripcion=True).get_data()
+
+        historical_date = CovidMX(date='04-12-2020', clean=False).get_data()
     except BaseException:
         assert False, "Test DGE failed"
