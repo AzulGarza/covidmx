19f5040716935a93950b7e9d1e1abe586dec2c33
Fede <fede.garza.ramirez@gmail.com>
2020-04-26 17:49:09 -0500

    preparing new release

--- setup.py
+++ setup.py
@@ -21,9 +21,14 @@ setuptools.setup(
     python_requires='>=3.5',
     install_requires = [
         "more-itertools>=6.0.0",
-        "pandas==0.25.2",
-        "Unidecode==1.1.1",
-        "requests==2.21.0",
-        "xlrd == 1.2.0"
+        "pandas>=0.25.2",
+        "Unidecode>=1.1.1",
+        "requests>=2.21.0",
+        "xlrd>=1.2.0",
+        "mapsmx>=0.0.3",
+        "matplotlib>=3.0.3",
+        "mapclassify>=2.2.0",
+        "descartes>=1.1.0",
+
     ]
 )
